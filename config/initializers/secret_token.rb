9dcb291bb0007d2834204912e4cb3f6e4a935b67
Jeremy Vandehey <jgvandehey@gmail.com>
2014-10-13 10:52:39 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jewelkick::Application.config.secret_token = 'd30df7d5ba5aedacb9e747dde5d97de544a4d700e9a0cb7a1f961a7a10fbca3d120794efb3da1a099963aad4b8b34e3d2a07a7fa00ec7f0e1ab947a56006ba3d'
